f5da8950cbfe487be49e118d39d29372cb6fbc07
Jacques Declercq <41050093+JacquesDeclercq@users.noreply.github.com>
2021-08-31 14:19:05 +0200

    DF Cleaning Part 2

--- df_cleaning.py
+++ df_cleaning.py
@@ -9,14 +9,7 @@ df_copy = pd.DataFrame(data)
 df = df_copy.T
 df = df.reset_index()
 
-df = df[['name','genres','is_free','price_overview','num_reviews',
-         'review_score','short_description','dlc',
-         'controller_support','platforms','developers','categories',
-        'website','screenshots','steam_appid','pc_requirements','mac_requirements','linux_requirements',
-        "release_date"]]
-
-## Function to flatten nested Json
-
+## Function to flatten nested JSON
 def flatten_json(nested_json, exclude=['']):
     """Flatten json object with nested keys into a single level.
         Args:
@@ -41,10 +34,6 @@ def flatten_json(nested_json, exclude=['']):
 
     flatten(nested_json)
     return out
-
-
-df1 = df[['name']]
-
 
 ### Creating the columns
 
@@ -72,7 +61,7 @@ df_releasedate = pd.DataFrame([flatten_json(x) for x in df['release_date']])
 
 df_genres = df_genres[['0_id', '0_description', '1_id', '1_description',]]
 df_genres = df_genres.drop(columns = ["0_id","1_id"], axis = 1)
-df_genres = df_genres.rename(columns={"0_description": "primary_genre", "1_description": "secundary_genre"})
+df_genres = df_genres.rename(columns={"0_description": "primary_genre", "1_description": "secondary_genre"})
 
 ## Price
 df_price = df_price[['currency','final']]
@@ -84,7 +73,7 @@ df_categories = df_categories[['0_id', '0_description', '1_id', '1_description',
 df_categories = df_categories.drop(columns =["0_id","1_id","2_id","2_description",
                                              "3_id","3_description","4_id","4_description"])
 
-df_categories = df_categories.rename(columns={"0_description": "primary_category", "1_description": "secundary_category"})
+df_categories = df_categories.rename(columns={"0_description": "primary_category", "1_description": "secondary_category"})
 
 ## Screenshots
 df_screenshots = df_screenshots[['0_path_full']]
@@ -110,7 +99,6 @@ df_linux['recommended'] =  df_linux['recommended'].str.replace(r"<[^>]*>","")
 df_linux = df_linux.rename(columns={"minimum": "linux_minimum", "recommended": "linux_recommended"})
 
 ## Release Date
-df_releasedate = pd.DataFrame([flatten_json(x) for x in df['release_date']])
 
 df_releasedate = df_releasedate.drop(columns = ["coming_soon"])
 
@@ -118,4 +106,6 @@ df_releasedate = df_releasedate.drop(columns = ["coming_soon"])
 frames = [df_genres,df_price,df_categories,df_screenshots,df_platforms,df_pc,df_mac,df_linux,df_releasedate]
 
 for frame in frames:
-    df1 = pd.concat([df1, frame], axis=1)
+    df = pd.concat([df, frame], axis=1)
+
+df = df.drop("date", axis=1)
